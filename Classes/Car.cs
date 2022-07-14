9f3cb98b84041b68f4bf398579f87cb4636094d5
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-13 20:30:46 -0400

    Created Car class

---
+++ Classes/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Classes
+{
+	public class Car
+	{
+		//Constructors
+		public Car()
+		{
+
+		}
+
+        public Car(string makeInput, string modelInput, int yearInput)
+        {
+			Make = makeInput;
+			Model = modelInput;
+			Year = yearInput;
+        }
+
+		//Properties
+		public string Make { get; set; } // get = read   set = write
+		public string Model { get; set; }
+		public int Year { get; set; }
+	}
+}
+
---
+++ Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿using Classes;
+
+Car myCar = new Car();
+myCar.Make = "Chevrolet";
+myCar.Model = "Camaro SS";
+myCar.Year = 1969;
+
+var yourCar = new Car() // Object intitialization
+{
+    Make = "Dodge",
+    Model = "Charger",
+    Year = 1970
+};
+
+var theirCar = new Car("Toyota", "Supra", 1993);
+
+var carList = new List<Car>() { myCar, yourCar, theirCar };
+
+foreach (var vehicle in carList)
+{
+    Console.WriteLine($"{vehicle.Year} {vehicle.Make} {vehicle.Model}");
+}
